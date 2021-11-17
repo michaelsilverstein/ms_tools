3e3e7438245e37d19d8c7959df4daaa4e67551c8
michaelsilverstein <michael.silverstein4@gmail.com>
2021-11-17 09:42:52 -0500

    bump

--- build/lib/ms_tools/viz.py
+++ build/lib/ms_tools/viz.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 def zebra(ax=None, orient='v', color='gray', alpha=.3, zorder=0, **kwargs):
     """
@@ -43,3 +44,35 @@ def zebra(ax=None, orient='v', color='gray', alpha=.3, zorder=0, **kwargs):
     ax.set_xlim(xlim)
     ax.set_ylim(ylim)
     return ax
+
+def stackedbarplot(data, stack_order=None, palette=None, **barplot_kws):
+    """
+    Create a stacked barplot
+    Inputs:
+    | data <pd.DataFrame>: A wideform dataframe where the index is the variable to stack, the columns are different samples (x-axis), and the cells the counts (y-axis)
+    | stack_order <array-like>: The order for bars to be stacked (Default: given order)
+    | palette <array-like>: The colors to use for each value of `stack_order` (Default: husl)
+    | barplot_kws: Arguments to pass to sns.barplot()
+    
+    Author: Michael Silverstein
+    Usage: https://github.com/michaelsilverstein/Pandas-and-Plotting/blob/master/lessons/stacked_bar_chart.ipynb
+    """
+    # Order df
+    if stack_order is None:
+        stack_order = data.index
+    # Create palette if none
+    if palette is None:
+        palette = dict(zip(stack_order, sns.husl_palette(len(stack_order))))
+    # Compute cumsum
+    cumsum = data.loc[stack_order].cumsum()
+    # Melt for passing to seaborn
+    cumsum_stacked = cumsum.stack().reset_index(name='count')
+    # Get name of variable to stack and sample
+    stack_name, sample_name = cumsum_stacked.columns[:2]
+    
+    # Plot bar plot
+    for s in stack_order[::-1]:
+        # Subset to this stack level
+        d = cumsum_stacked[cumsum_stacked[stack_name].eq(s)]
+        sns.barplot(x=sample_name, y='count', hue=stack_name, palette=palette, data=d, **barplot_kws)
+    return plt.gca()
