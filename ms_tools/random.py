e03c374fed8c2b0d574f84838deeea9b2335f515
michaelsilverstein <michael.silverstein4@gmail.com>
2024-06-05 08:54:57 -0400

    rename probability vars

--- ms_tools/random.py
+++ ms_tools/random.py
@@ -2,7 +2,7 @@
 import numpy as np
 import pandas as pd
 
-def structured_binary_matrix(row_group_sizes, col_group_sizes, association_matrix, group_sparsity=.5, noise=0, row_group_name=None, col_group_name=None, row_name=None, col_name=None):
+def structured_binary_matrix(row_group_sizes, col_group_sizes, association_matrix, group_probability=.5, nongroup_probability=0, row_group_name=None, col_group_name=None, row_name=None, col_name=None):
     """Generate a structured binary matrix with specified associations between groups in rows and columns
     with tunable amounts of noise for group membership
 
@@ -11,10 +11,10 @@ def structured_binary_matrix(row_group_sizes, col_group_sizes, association_matri
             sizes 100, 50, and 25, provide: [100, 50, 25]
         association_matrix (array): A n_rows X n_cols binary array specifying the associations between
             the rows and columns.
-        group_sparsity (float): The probability of belonging to an associated group. A `group_sparsity`
+        group_probability (float): The probability of belonging to an associated group. A `group_sparsity`
         of 0 will result in no grouped entries and a `group_sparsity` of 1 will sample all group entries.
             Default = 0.5
-        noise (float): The probability of non-group entries.
+        nongroup_probability (float): The probability of non-group entries.
             Default = 0.
         {row, col}{_group}_name (str): Name of group and individual row and column entries.
         
@@ -23,8 +23,6 @@ def structured_binary_matrix(row_group_sizes, col_group_sizes, association_matri
     """
     
     "Inputs"
-    group_sparsity = 1 - group_sparsity
-    
     n_groups_col, n_groups_row = map(len, (col_group_sizes, row_group_sizes))
     n_cols, n_rows = map(sum, (col_group_sizes, row_group_sizes))
     association_matrix = np.array(association_matrix)
@@ -63,11 +61,11 @@ def structured_binary_matrix(row_group_sizes, col_group_sizes, association_matri
     row_groups = row_idx.get_level_values(row_group_name).unique()
     row_groups_to_col_groups = dict(zip(row_groups, [col_groups[a] for a in association_matrix]))
     
-    # Apply group sparsity and noise for each row-col group pairing
+    # Apply group and nongroup probability for each row-col group pairing
     for rg, cg in row_groups_to_col_groups.items():
-        M.loc[rg, cg] = group_sparsity
+        M.loc[rg, cg] = group_probability
         other_cg = list(set(col_groups).difference(set(cg)))
-        M.loc[rg, other_cg] = noise
+        M.loc[rg, other_cg] = nongroup_probability
     
     "Sample entries"
     x = np.random.rand(n_rows, n_cols) < M
